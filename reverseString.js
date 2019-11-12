174e0f2e9e1274ce49dd87618f8cf9cfe8ef2e43
PJMantoss <joelptoss@gmail.com>
2019-11-12 18:57:35 +0100

    Add reverseArrayInPlace algorithm

--- reverseString.js
+++ reverseString.js
@@ -1,18 +1,20 @@
-// How to reverse a string in JS
-reverseStr = str => {
-    if (str === ""){
-        return "";
-    } else {
-        return reverse(str.substr(1)) + str.charAt(0);
-    }
-}
-
 // How to reverse words in a sentence
 reverseWord = str => {
-    return str.split('').reverse();
+    return str.split('').reverse().join('');
 }
 
 //How to reverse words in place
 reverseInPlace = str => {
-    return str.split('').reverse().join('').split('').reverse().join('');
+    return str.split(' ').reverse().join(' ').split('').reverse().join('');
+}
+
+// How to Reverse Array in Place
+reverseArrayInPlace = arr => {
+    for (let i=0; i<arr.length/2; i++){
+        tempValue = arr[i];
+        arr[i] = arr[arr.length - 1 - i];
+        arr[arr.length - 1 - i] = tempValue;
+    }
+
+    return arr;
 }
