2f16c5cced6b5a3819d774531e9aa619ef7c52a0
PJMantoss <joelptoss@gmail.com>
2019-11-16 15:36:06 +0100

    Add max stock profit algorithm solution

---
+++ maxStockProfit.js
@@ -0,0 +1,33 @@
+/*
+The cost of a stock on each day is given in an array, find the max profit that you can make by buying and selling in those days. 
+For example, if the given array is {32, 46, 26, 38, 40, 48, 42}, the maximum profit that can be earned is 22. 
+This is when you buy on day 3 (26) and sell on day 6 (48).
+
+RULES:
+  Takes in array of prices as parameters,
+  Returns max possible profit of the day,
+  If there is no possible profit, return -1,
+  Else, return profit.
+ */
+
+ maxStockProfit = prices => {
+     let maxProfit = -1, 
+         buyPrice = 0, 
+         sellPrice = 0,
+         changeBuyPrice = true;
+
+         for (let i=0; i<prices.length; i++){
+             if(changeBuyPrice) buyPrice = prices[i];
+               sellPrice = prices[i + 1];
+
+               if (sellPrice < buyPrice){
+                changeBuyPrice = true;
+              }else {
+                let tempProfit = sellPrice - buyPrice;
+                if (tempProfit > maxProfit) maxProfit = tempProfit;
+                changeBuyPrice = false;
+            }
+         } 
+
+         return maxProfit;
+ }
--- sieveOfEratosthenes.js
+++ sieveOfEratosthenes.js
@@ -2,6 +2,28 @@
 
 /*RULES:
   Function takes one parameter
-  Return an array of all prime numbers from 0-parameter
+  Returns an array of all prime numbers from 0-parameter
 */
 //Solution
+sieveOfEratosthenes = n => {
+  const isPrime = new Array(n + 1).fill(true);
+
+  isPrime[0] = false;
+  isPrime[1] = false;
+
+  const primes = [];
+
+  for (let num = 2; num < n; num++){
+      if (isPrime[num] === true){
+          primes.push(num);
+
+          let nextNum = num * num;
+
+          while (nextNum <= n){
+              isPrime[nextNum] = false;
+              nextNum += num;
+          }
+      }
+  }
+  return primes;
+}
