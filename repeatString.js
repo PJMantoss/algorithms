bae5f33a8a7768469a5b790fa1fbee52066f83e4
PJMantoss <joelptoss@gmail.com>
2019-12-15 15:18:30 +0100

    Add algorithm

---
+++ repeatString.js
@@ -0,0 +1,14 @@
+function repeatStringNumTimes(str, num) {
+    // This solution uses recursion
+    if (num < 1){ // We check if a number is negative and return empty str if true
+      return "";
+    } else if(num === 1) { // If number equals to 1 we return the string itself
+      return str;
+    } else { // If not we add the string to a call of our function reducing num by 1
+      return str + repeatStringNumTimes(str, num - 1);
+    }
+    
+  }
+  
+  repeatStringNumTimes("abc", 3);
+  
