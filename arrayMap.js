9f373b6133d2e7d2d5a92baa63f329a23f211f77
pjmantoss <joelptoss@gmail.com>
2020-12-29 14:59:32 +0100

    update arrayMap

--- arrayMap.js
+++ arrayMap.js
@@ -1,6 +1,10 @@
 //Implement your own array mapping function with reduce()
 //NOTE: You are only allowed to use the array.reduce() method
 
-const map = (array, callback) => {}
+const map = (array, callback) => {
+    return array.reduce((accumulator, value) => {
+        return [...accumulator, callback(value)];
+    }, [])
+}
 
-console.log([1,2,3], (v) => v + 1); [2,3,4]
+console.log([1,2,3], (v) => v + 1); //[2,3,4]
