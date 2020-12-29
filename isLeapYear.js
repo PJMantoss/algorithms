7ed9cd2441f2906f0c8066310137be8e93944ffe
pjmantoss <joelptoss@gmail.com>
2020-12-29 10:42:19 +0100

    add isLeapYear

---
+++ isLeapYear.js
@@ -0,0 +1,8 @@
+//Implement isLeapYear function
+//Leap year occurs every 4 years except for century years, but not when the year is divisible by 400.
+
+const isLeapYear = year => {
+    const numYear = Number(year);
+
+    return numYear % 100 === 0 ? numYear % 400 === 0 : numYear % 4 === 0;
+}
