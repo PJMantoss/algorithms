d869ad33fd1d166cefb971b689969f6254228a65
pjmantoss <joelptoss@gmail.com>
2020-12-29 09:33:23 +0100

    update isPangram

--- isPangram.js
+++ isPangram.js
@@ -6,5 +6,11 @@ const isPangram = string => {
     //remove all capital letters
      //remove spaces between words and join the words without comma
      //filter to remove duplicates and sort
-    const processedString = [...new Set(string.toLowerCase().split(' ').join(''))];
+    const processedString = [...new Set(string.toLowerCase().split(' ').join(''))].sort().join('');
+
+    return alphabets === processedString;
 }
+
+console.log(isPangram('the quick brown fox jumps over the lazy dog')); //true
+console.log(isPangram('abcdefghijklmnopqrstuvwxyz')); //true
+console.log(isPangram('abcdefhijklmopqrstuvwz')); //false
