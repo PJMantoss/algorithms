4ae6fe3b2f6783073c97625c92927df59e556152
pjmantoss <joelptoss@gmail.com>
2020-12-29 14:57:50 +0100

    add arrayMap

---
+++ arrayMap.js
@@ -0,0 +1,6 @@
+//Implement your own array mapping function with reduce()
+//NOTE: You are only allowed to use the array.reduce() method
+
+const map = (array, callback) => {}
+
+console.log([1,2,3], (v) => v + 1); [2,3,4]
