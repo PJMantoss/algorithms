b3ebf350e73f83f550b9978a838251d00c27dcc6
pjmantoss <joelptoss@gmail.com>
2020-12-29 10:21:59 +0100

    update toRainLanguage

--- toRainLanguage.js
+++ toRainLanguage.js
@@ -4,4 +4,11 @@ const toRainLangusge = number => {
     if(number % 3 === 0) result += "Pling";
     if(number % 5 === 0) result += "Plang";
     if(number % 7 === 0) result += "Plong";
+
+    return result || number;
 }
+
+console.log(toRainLangusge(3));
+console.log(toRainLangusge(21));
+console.log(toRainLangusge(35));
+console.log(toRainLangusge(1));
