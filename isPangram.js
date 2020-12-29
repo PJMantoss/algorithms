38f9d3ddf8ed0739bb2dc9d5966fc41175a79a86
pjmantoss <joelptoss@gmail.com>
2020-12-29 09:23:03 +0100

    update isPangram

--- isPangram.js
+++ isPangram.js
@@ -1,1 +1,9 @@
 //A function that checks if a sentence contains all the letters of the alphabets
+
+const alphabets = 'abcdefghijklmnopqrstuvwxyz';
+
+const isPangram = string => {
+    //remove all capital letters
+    const processedString = string.toLowerCase();
+    //
+}
